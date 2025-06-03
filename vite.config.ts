0d32bdabc58a2e11df8fe67eb183c3d7c0fc9056
edgarMay <tu-email@ejemplo.com>
2025-06-02 19:51:18 -0600

    Guardar cambios antes de crear el gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolioEM/',
-  plugins: [react()],
+  // Si vas a usar un dominio propio, sirve todo desde la raíz:
+  base: '/',                // ← ahora asumimos que los assets estarán en `/assets/...`
+  plugins: [react()]
 })
