0ba1e9789ea59e0c7523fb88a911fb8b857b71d1
edgarMay <tu-email@ejemplo.com>
2025-06-04 17:14:17 -0600

    Eliminar CNAME de github pages y preparar despliegue en netlify

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -13,9 +13,9 @@ export default function ContactMe() {
   })
 
   // 2) Tus IDs de EmailJS
-  const SERVICE_ID  = 'service_2pjeya3'
-  const TEMPLATE_ID = 'template_vn6gq5q'
-  const PUBLIC_KEY  = 'qfSfduvF4liy9nVY2'
+  const SERVICE_ID  = import.meta.env.VITE_EMAILJS_SERVICE_ID
+  const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+  const PUBLIC_KEY  = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 
   // 3) Inicializa EmailJS
   useEffect(() => {
